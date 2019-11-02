ab6f2e11dab43f03f8de5ade239c221ac78a571d
cin_hlossolli <cin_hlossolli@uolinc.com>
2019-11-02 17:59:38 -0300

    all files

--- forca.py
+++ forca.py
@@ -3,7 +3,31 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "potato"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+
+
+    while (not enforcou and not acertou):
+
+        print("Palavra: ", letras_acertadas)
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+
+            index = index + 1
+
+        letras_faltando = str(letras_acertadas.count("_"))
+        print("Ainda faltam acertar {} letras!".format(letras_faltando))
+
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     jogar()
